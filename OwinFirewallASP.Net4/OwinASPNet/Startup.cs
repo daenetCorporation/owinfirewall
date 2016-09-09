fa0c7e4cb7ce41b8a61db80da5bb7c91425f26ac
Ataulhaye <ataulhaye1@gmail.com>
2016-09-09 15:34:09 +0200

    folder name changed

--- OwinFirewallASP.Net4/OwinASPNet/Startup.cs
+++ OwinFirewallASP.Net4/OwinASPNet/Startup.cs
@@ -13,7 +13,7 @@ namespace OwinASPNet
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseOwinFirewall(new Daenet.Owin.OwinFirewallOptions(new List<string>() { "79.206.242.74", "::1", "localhost", "212.144.228.0/24", "62.96.6.0/27" }));
+            app.UseOwinFirewall(new Daenet.Owin.OwinFirewallOptions(new List<string>() { "79.206.242.74", "::21", "localhost", "212.144.228.0/24", "62.96.6.0/27" }));
 
             app.Use((context, next) =>
             {
